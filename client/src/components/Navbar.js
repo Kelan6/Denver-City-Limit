01353411634d6ad7b02aef9321a8efe4c0cc88a4
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-09-13 19:29:52 -0600

    weird nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,13 +6,15 @@ function NavBar() {
 
     return (
         <div id="navbar">
-            <span id="banner">
-                <Link className="route-link" to="/home"> DCL </Link>
+            <div id='bannerDCL'>
+            <Link className="route-link" to="/home"> DCL </Link>
+            </div>
+            <div id="banner">
                 <Link className="route-link" to="/lineup">Lineup</Link>
                 <Link className="route-link" to="/schedule">Schedule</Link>
                 <Link className="route-link" to="/tickets">Tickets</Link>
                 <Link className="route-link" to="/login">Login</Link>
-            </span>
+            </div>
         </div>
     );
 };
